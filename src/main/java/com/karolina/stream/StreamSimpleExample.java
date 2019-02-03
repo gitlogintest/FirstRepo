addc454468fd647d24709079668759e5272b3e8e
gitlogintest <dopytan@gmail.com>
2019-02-03 20:09:58 +0100

    new enum 2

--- src/main/java/com/karolina/stream/StreamSimpleExample.java
+++ src/main/java/com/karolina/stream/StreamSimpleExample.java
@@ -12,7 +12,7 @@ import java.util.stream.Stream;
 public class StreamSimpleExample {
     private static Employee[] arrayOfEmps = {
             new Employee(1, "Jeff Bezos", 100000.0),
-            new Employee(2, "Bill Gates", 200000.0),
+            new Employee(2, "Bill Gates1", 200000.0),
             new Employee(3, "Mark Zuckerberg", 300000.0),
             new Employee(4, "Mark fff", 500000.0)
     };
