592c0b8c8bc592049c48acff0221e1f7cdd97757
Pawel <dopytan@gmail.com>
2019-02-03 19:16:00 +0100

    first commit

---
+++ src/main/java/com/karolina/stream/StreamSimpleExample.java
@@ -0,0 +1,86 @@
+package com.karolina.stream;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by Paweł on 2019-01-27.
+ */
+public class StreamSimpleExample {
+    private static Employee[] arrayOfEmps = {
+            new Employee(1, "Jeff Bezos", 100000.0),
+            new Employee(2, "Bill Gates", 200000.0),
+            new Employee(3, "Mark Zuckerberg", 300000.0)
+    };
+    private static List<Employee> empList = Arrays.asList(arrayOfEmps);
+    public static void main(String[] args){
+/*
+
+        System.out.println(Stream.of(arrayOfEmps));
+
+        empList.stream().forEach(employee -> employee.salaryIncrement(10.0));
+       Employee emp = empList.get(0);
+        System.out.println( emp.getName() +", " +emp.getSalary());
+
+        Employee[] arrayOfEmps = {
+                new Employee(1, "Jeff Bezos", 100000.0),
+                new Employee(2, "Bill Gates", 200000.0),
+                new Employee(3, "Mark Zuckerberg", 300000.0)
+        };*/
+
+        List<Employee> empList = Arrays.asList(arrayOfEmps);
+
+        empList.stream()
+                .peek(e -> e.salaryIncrement(10.0))
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+
+        Stream<Integer> infiniteStream = Stream.iterate(2, i -> i * 2);
+
+        List<Integer> collect = infiniteStream
+                .skip(3)
+                .limit(5)
+                .collect(Collectors.toList());
+
+
+
+    }
+}
+
+class Employee {
+    public int getNumber() {
+        return number;
+    }
+
+    int number;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    String name;
+    double salary;
+
+    Employee(int number, String name, double salary) {
+        this.name = name;
+        this.number = number;
+        this.salary = salary;
+
+    }
+
+
+    public void salaryIncrement(double v) {
+        this.salary += v;
+    }
+
+    public  String toString() {
+        return this.name+", "+ this.number +", "+ this.salary;
+    }
+}
---
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -0,0 +1,36 @@
+package com.pluralsight.model;
+
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class Goal {
+
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Range(min = 1, max = 120)
+	private int minutes;
+
+	public int getMinutes() {
+		return minutes;
+	}
+
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
